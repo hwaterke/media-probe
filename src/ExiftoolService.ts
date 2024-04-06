2dd6fc58cb47220f46b92ba0885941b2fabc9083
hwaterke <hwaterke@users.noreply.github.com>
2024-04-06 18:12:47 +0200

    Adds methods to write exif data

--- src/ExiftoolService.ts
+++ src/ExiftoolService.ts
@@ -11,6 +11,7 @@ import {
   EXIF_DATE_TIME_SUBSEC_WITH_TZ_REGEX,
   EXIF_DATE_TIME_WITH_TZ_REGEX,
   EXIF_DATE_TIME_WITH_UTC_REGEX,
+  TZ_OFFSET_REGEX,
 } from './utils.js'
 import {EXIF_TAGS, ExiftoolMetadata} from './types/ExiftoolMetadata.js'
 import {DateTime} from 'luxon'
@@ -30,6 +31,21 @@ export class ExiftoolService {
   async extractExifMetadata(path: string): Promise<ExiftoolMetadata> {
     const rawResult = await this.exiftool({
       args: ['-G0:1', '-json'],
+      path,
+      options: {
+        override: false,
+        ignoreMinorErrors: false,
+      },
+    })
+    return JSON.parse(rawResult)[0]
+  }
+
+  /**
+   * Returns the time related exif metadata stored on the file provided
+   */
+  async extractTimeExifMetadata(path: string): Promise<ExiftoolMetadata> {
+    const rawResult = await this.exiftool({
+      args: ['-Time:All', '-api QuickTimeUTC', '-G0:1', '-json'],
       path,
       options: {
         override: false,
@@ -111,6 +127,129 @@ export class ExiftoolService {
     }
 
     return null
+  }
+
+  async extractGpsExifMetadata(path: string): Promise<{
+    latitude: number
+    longitude: number
+  } | null> {
+    const rawResult = await this.exiftool({
+      args: ['-GPSLatitude', '-GPSLongitude', '-json', '-c %.6f'],
+      path,
+      options: {
+        override: false,
+        ignoreMinorErrors: false,
+      },
+    })
+
+    const result = JSON.parse(rawResult)[0]
+    const gpsLatitudeStr: string | undefined | null = result.GPSLatitude
+    const gpsLongitudeStr: string | undefined | null = result.GPSLongitude
+    const gpsLatitudeParts = gpsLatitudeStr?.match(/([\d.]+)\s*([ENSW])/)
+    const gpsLongitudeParts = gpsLongitudeStr?.match(/([\d.]+)\s*([ENSW])/)
+
+    if (!gpsLatitudeParts || !gpsLongitudeParts) {
+      return null
+    }
+
+    const latitude =
+      (gpsLatitudeParts[2] === 'S' ? -1 : 1) *
+      Number.parseFloat(gpsLatitudeParts[1])
+    const longitude =
+      (gpsLongitudeParts[2] === 'W' ? -1 : 1) *
+      Number.parseFloat(gpsLongitudeParts[1])
+
+    return {
+      latitude,
+      longitude,
+    }
+  }
+
+  async setQuickTimeCreationDate(
+    path: string,
+    time: string,
+    options: {
+      override: boolean
+      ignoreMinorErrors: boolean
+    }
+  ): Promise<void> {
+    // QuickTime CreationDate is set on Apple videos. As it contains the TZ it is the most complete field possible.
+    if (!EXIF_DATE_TIME_WITH_TZ_REGEX.test(time)) {
+      throw new Error(
+        `Invalid time provided ${time}. Please use ${EXIF_DATE_TIME_WITH_TZ_REGEX}`
+      )
+    }
+
+    await this.exiftool({
+      args: ['-api QuickTimeUTC', '-P', `-quicktime:CreationDate="${time}"`],
+      path,
+      options,
+    })
+  }
+
+  /**
+   * Sets all the times to the provided time.
+   * Can also change the file attributes to be in sync.
+   */
+  async setAllTime(
+    path: string,
+    time: string,
+    options: {
+      file: boolean
+      override: boolean
+      ignoreMinorErrors: boolean
+    }
+  ): Promise<void> {
+    if (
+      !EXIF_DATE_TIME_WITH_TZ_REGEX.test(time) &&
+      !EXIF_DATE_TIME_SUBSEC_WITH_TZ_REGEX.test(time)
+    ) {
+      throw new Error(
+        `Invalid time provided ${time}. Please use ${EXIF_DATE_TIME_WITH_TZ_REGEX} or ${EXIF_DATE_TIME_SUBSEC_WITH_TZ_REGEX}`
+      )
+    }
+
+    await this.exiftool({
+      args: [
+        '-api QuickTimeUTC',
+        '-wm w',
+        `-time:all="${time}"`,
+        options.file
+          ? `-FileCreateDate="${time}" -FileModifyDate="${time}"`
+          : '-P',
+      ],
+      path,
+      options,
+    })
+  }
+
+  /**
+   * Sets the time zone offset in the exif data (for photos)
+   */
+  async setTimezoneOffsets(
+    path: string,
+    offset: string,
+    options: {
+      override: boolean
+      ignoreMinorErrors: boolean
+    }
+  ): Promise<void> {
+    if (!TZ_OFFSET_REGEX.test(offset)) {
+      throw new Error(
+        `Invalid offset provided ${offset}. Please use ${EXIF_DATE_TIME_WITH_TZ_REGEX}`
+      )
+    }
+
+    await this.exiftool({
+      args: [
+        '-P',
+        `-OffsetTime="${offset}"`,
+        `-OffsetTimeOriginal="${offset}"`,
+        `-OffsetTimeDigitized="${offset}" "${path}"`,
+      ],
+      path,
+      options,
+    })
   }
 
   async exiftool({
