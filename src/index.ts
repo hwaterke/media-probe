1ae452051560e237064d6827062cb86e2b8194fc
hwaterke <hwaterke@users.noreply.github.com>
2025-05-09 20:43:42 +0200

    Adds missing format

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export {ExiftoolService} from './ExiftoolService.js'
 export {EXIF_TAGS} from './types/ExiftoolMetadata.js'
 export type {ExiftoolMetadata} from './types/ExiftoolMetadata.js'
 export {
+  EXIF_DATE_TIME_FORMAT,
   EXIF_DATE_TIME_FORMAT_WITH_TZ,
   EXIF_DATE_TIME_SUBSEC_FORMAT,
   EXIF_DATE_TIME_SUBSEC2_FORMAT_WITH_TZ,
